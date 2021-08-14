8388f95a33777b31126f9c2a4145b084643dc77a
himanshu <himanshu.chauhan@dyt.onl>
2021-08-14 22:02:04 +0530

    checking last time

--- abcd.py
+++ abcd.py
@@ -3,8 +3,9 @@ def greeting():
     print("Himanshu")
     if 1:
         print("hey")
-        print("this will create bommbbb!!!!")
+    print("this will create bommbbb!!!!")
     print("this will create space issue i think")
+    print("one more last time")
 
 
 greeting()
